7660377c4a773340f1b8b37fb70e619633b0d39f
Phil Weir <phil.weir@flaxandteal.co.uk>
2020-03-12 23:04:26 +0000

    minor improvements for robustness and scikit deprecation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tablespy',
-    version='0.0.3',
+    version='0.0.4',
     description='Table spotter for spreadsheets',
     url='https://github.com/flaxandteal/tablespy',
     author='Phil Weir',
